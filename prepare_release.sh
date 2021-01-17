4473b7e5394e7942aeab01e80f752b8c7248f847
Suraj Shrestha <syuraj@gmail.com>
2021-01-17 15:51:57 -0500

    minor sed replace command correction

--- prepare_release.sh
+++ prepare_release.sh
@@ -12,7 +12,7 @@ GOOGLE_SERVICES_FILE=./android/app/google-services.json
 
 sed -i '' -e 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
 sed -i '' -e 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
-sed -i '' -e 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
+sed -i '' -e "s/\"app_name\">[^<]*</\"app_name\">$APP_NAME</" $ANDROID_STRINGS_FILE
 
 sed -i '' -e 's/"ic_launcher_background">[^<]*</"ic_launcher_background">'$IC_LAUNCHER_BACKGROUND'</' $IC_LAUNCHER_BACKGROUND_FILE
 
