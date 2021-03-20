0557a640ef9b161f1da9ddc877b6a42db7b51cd0
Suraj Shrestha <syuraj@gmail.com>
2021-03-20 19:25:45 -0400

    show app icon on first login screen

--- src/components/screens/login/components/MiddleContent.js
+++ src/components/screens/login/components/MiddleContent.js
@@ -1,72 +1,66 @@
-// @flow
-
-import React from 'react';
-import { Platform, View, Text } from 'react-native';
-import styled from 'styled-components';
-
-import Icon from '~/components/common/Icon';
-import appStyles from '~/styles';
+import React from 'react'
+import { Platform, View, Text } from 'react-native'
+import FastImage from 'react-native-fast-image'
+import styled from 'styled-components'
+import appIconImg from '~/../android/app/src/main/ic_launcher-playstore.png'
 
 const Wrapper = styled(View)`
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  padding-horizontal: ${({ theme }) => theme.metrics.extraLargeSize}px;
-`;
+	width: 100%;
+	justify-content: center;
+	align-items: center;
+	padding-horizontal: ${({ theme }) => theme.metrics.extraLargeSize}px;
+`
 
 const IconWrapper = styled(View)`
-  width: ${({ theme }) => theme.metrics.getWidthFromDP('50%')}px;
-  height: ${({ theme }) => theme.metrics.getWidthFromDP('50%')}px;
-  justify-content: center;
-  align-items: center;
-  padding-top: ${({ theme }) => (Platform.OS === 'ios' ? theme.metrics.mediumSize : 0)}px;
-  border-radius: ${({ theme }) => theme.metrics.getWidthFromDP('25%')}px;
-  background-color: ${({ theme }) => theme.colors.primaryColor};
-`;
+	width: ${({ theme }) => theme.metrics.getWidthFromDP('50%')}px;
+	height: ${({ theme }) => theme.metrics.getWidthFromDP('50%')}px;
+	justify-content: center;
+	align-items: center;
+	padding-top: ${({ theme }) => (Platform.OS === 'ios' ? theme.metrics.mediumSize : 0)}px;
+	border-radius: ${({ theme }) => theme.metrics.getWidthFromDP('25%')}px;
+	background-color: #e7f2fe;
+`
+
+const AppIcon = styled(FastImage).attrs(({ source }) => ({
+	source: source,
+}))`
+	width: ${({ theme }) => theme.metrics.getWidthFromDP('40%')}px;
+	height: ${({ theme }) => theme.metrics.getWidthFromDP('40%')}px;
+	border-radius: ${({ theme }) => theme.metrics.getWidthFromDP('8.5%')}px;
+`
 
 const Title = styled(Text)`
-  margin-top: ${({ theme }) => 1.2 * theme.metrics.extraLargeSize}px;
-  margin-bottom: ${({ theme }) => theme.metrics.mediumSize}px;
-  color: ${({ theme }) => theme.colors.darkText};
-  font-size: ${({ theme }) => 1.3 * theme.metrics.extraLargeSize};
-  font-family: CircularStd-Black;
-`;
+	margin-top: ${({ theme }) => 1.2 * theme.metrics.extraLargeSize}px;
+	margin-bottom: ${({ theme }) => theme.metrics.mediumSize}px;
+	color: ${({ theme }) => theme.colors.darkText};
+	font-size: ${({ theme }) => 1.3 * theme.metrics.extraLargeSize};
+	font-family: CircularStd-Black;
+`
 
 const Description = styled(Text)`
-  font-size: ${({ theme }) => 1.2 * theme.metrics.largeSize}px;
-  font-family: CircularStd-Medium;
-  color: ${({ theme }) => theme.colors.subTextWhite};
-  text-align: center;
-`;
+	font-size: ${({ theme }) => 1.2 * theme.metrics.largeSize}px;
+	font-family: CircularStd-Medium;
+	color: ${({ theme }) => theme.colors.subTextWhite};
+	text-align: center;
+`
 
-const ITEMS = [
-	{
-		title: 'NepalToday Podcast',
-		description: 'Download your favorite podcasts and playlists to listen offline.',
-		icon: 'headphones',
-	},
-]
+const SCREEN_TEXT = {
+	title: 'NepalToday Podcast',
+	description: 'Download your favorite podcasts on Nepali news, politics, interviews, stories to listen offline.',
+}
 
-type Props = {
-  currentIndex: number,
-};
+const MiddleContent = () => {
+	const { title, description } = SCREEN_TEXT
 
-const MiddleContent = ({ currentIndex }: Props): Object => {
-  const { title, description, icon } = ITEMS[currentIndex];
+	return (
+		<Wrapper>
+			<IconWrapper>
+				<AppIcon source={appIconImg} />
+			</IconWrapper>
+			<Title>{title}</Title>
+			<Description>{description}</Description>
+		</Wrapper>
+	)
+}
 
-  return (
-    <Wrapper>
-      <IconWrapper>
-        <Icon
-          color={appStyles.colors.white}
-          name={icon}
-          size={100}
-        />
-      </IconWrapper>
-      <Title>{title}</Title>
-      <Description>{description}</Description>
-    </Wrapper>
-  );
-};
-
-export default MiddleContent;
+export default MiddleContent
