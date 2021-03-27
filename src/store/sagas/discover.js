8ee629d31e5ab93b8f8ecbf8c6341773dbd8c0cb
Suraj Shrestha <syuraj@gmail.com>
2021-03-27 02:48:52 -0400

    Using graphql to fetch programs

--- src/store/sagas/discover.js
+++ src/store/sagas/discover.js
@@ -1,28 +1,25 @@
-import { call, put } from 'redux-saga/effects'
+import { put } from 'redux-saga/effects'
+import gql from 'graphql-tag'
 
-import api from '~/services/api'
-
-import { getItemFromStorage } from '../../utils/AsyncStorageManager'
+import client from '../../graphql/graphql-client'
 import { Creators as DiscoverCreators } from '../ducks/discover'
-import CONSTANTS from '../../utils/CONSTANTS'
-import parseParams from './utils/parseParams'
 
 export function* getDiscover() {
 	try {
-		const rawInterests = yield call(getItemFromStorage, CONSTANTS.KEYS.INTERESTS_STORAGE_KEY, [])
-
-		const interests = typeof rawInterests === 'string' ? JSON.parse(rawInterests) : rawInterests
-
-		let interestsSelected = interests.filter((interest) => interest.isSelected).map((interest) => interest.title.toLowerCase())
-
-		interestsSelected = 'all'
-
-		const { data } = yield call(api.get, '/programs', {
-			paramsSerializer: (params) => parseParams(params),
-			params: { categories: interestsSelected },
+		const result = yield client.query({
+			query: gql`
+				query homeScreenQuery {
+					getAllPrograms {
+						id
+						title
+						imageUrl
+						category
+					}
+				}
+			`,
 		})
 
-		yield put(DiscoverCreators.getDiscoverSuccess(data))
+		yield put(DiscoverCreators.getDiscoverSuccess(result.data.getAllPrograms))
 	} catch (err) {
 		yield put(DiscoverCreators.getDiscoverFailure())
 	}
