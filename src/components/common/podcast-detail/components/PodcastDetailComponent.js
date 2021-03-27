3d2f0269ef86d411935f56d473c2ad1329a2c91b
Suraj Shrestha <syuraj@gmail.com>
2021-03-27 03:59:46 -0400

    podcast detail screen fix

--- src/components/common/podcast-detail/components/PodcastDetailComponent.js
+++ src/components/common/podcast-detail/components/PodcastDetailComponent.js
@@ -1,105 +1,89 @@
-// @flow
+import React from 'react'
+import { ScrollView } from 'react-native'
+import styled from 'styled-components'
 
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
-import FastImage from 'react-native-fast-image';
-import styled, { withTheme } from 'styled-components';
+import PlaylistList from '~/components/common/playlists-list/PlaylistsListContainer'
 
-import PlaylistList from '~/components/common/playlists-list/PlaylistsListContainer';
+import ActionButtons from './ActionButtons'
+import BottomContent from './BottomContent'
+import PodcastInfo from './PodcastInfo'
 
-import ActionButtons from './ActionButtons';
-import BottomContent from './BottomContent';
-import PodcastInfo from './PodcastInfo';
-
-import CONSTANTS from '~/utils/CONSTANTS';
+import CONSTANTS from '~/utils/CONSTANTS'
 
 const Wrapper = styled(ScrollView)`
-  width: 100%;
-  height: 100%;
-  flex: 1;
-  padding: ${({ theme }) => theme.metrics.mediumSize}px;
-  background-color: ${({ theme }) => theme.colors.backgroundColor};
-`;
+	width: 100%;
+	height: 100%;
+	flex: 1;
+	padding: ${({ theme }) => theme.metrics.mediumSize}px;
+	background-color: ${({ theme }) => theme.colors.backgroundColor};
+`
 
 type AuthorProps = {
-  smallImageURL: string,
-  name: string,
-  id: string,
-};
+	smallImageURL: string,
+	name: string,
+	id: string,
+}
 
 type PodcastProps = {
-  author: AuthorProps,
-  description: string,
-  uploadedAt: string,
-  imageURL: string,
-  subject: string,
-  title: string,
-  stars: number,
-  url: string,
-  id: string,
-};
+	author: AuthorProps,
+	description: string,
+	uploadedAt: string,
+	imageURL: string,
+	subject: string,
+	title: string,
+	stars: number,
+	url: string,
+	id: string,
+}
 
 type Props = {
-  onToggleAddPlaylistModal: Function,
-  onNavigateAuthorDetail: Function,
-  shouldShowAuthorSection: boolean,
-  isAddPlaylistModalOpen: boolean,
-  onPressDownloadButton: Function,
-  isDownloadingPodcast: boolean,
-  isPodcastDownloaded: boolean,
-  onPressPlay: Function,
-  navigation: Object,
-  podcast: Props,
-};
+	onToggleAddPlaylistModal: Function,
+	onNavigateAuthorDetail: Function,
+	shouldShowAuthorSection: boolean,
+	isAddPlaylistModalOpen: boolean,
+	onPressDownloadButton: Function,
+	isDownloadingPodcast: boolean,
+	isPodcastDownloaded: boolean,
+	onPressPlay: Function,
+	navigation: Object,
+	podcast: Props,
+}
 
 const PodcastDetailComponent = ({
-  onToggleAddPlaylistModal,
-  shouldShowAuthorSection,
-  onNavigateAuthorDetail,
-  isAddPlaylistModalOpen,
-  onPressDownloadButton,
-  isDownloadingPodcast,
-  isPodcastDownloaded,
-  onPressPlay,
-  navigation,
-  podcast,
+	onToggleAddPlaylistModal,
+	shouldShowAuthorSection,
+	onNavigateAuthorDetail,
+	isAddPlaylistModalOpen,
+	onPressDownloadButton,
+	isDownloadingPodcast,
+	isPodcastDownloaded,
+	onPressPlay,
+	navigation,
+	podcast,
 }: Props): Object => (
-  <Wrapper
-    showsVerticalScrollIndicator={false}
-    alwaysBounceVertical={false}
-  >
-    <PodcastInfo
-      imageURL={podcast.imageURL}
-      subject={podcast.category}
-      title={podcast.title}
-      stars={4}
-    />
-    <ActionButtons
-      onPressAddToPlaylist={onToggleAddPlaylistModal}
-      isDownloadingPodcast={isDownloadingPodcast}
-      onPressDownloadButton={onPressDownloadButton}
-      isPodcastDownloaded={isPodcastDownloaded}
-      onPressPlay={onPressPlay}
-    />
-    <BottomContent
-      shouldShowAuthorSection={shouldShowAuthorSection}
-      onPressDetail={() => {
-        navigation.navigate(CONSTANTS.ROUTES.AUTHOR_DETAIL, {
-          [CONSTANTS.PARAMS.AUTHOR_DETAIL]: {
-            program: podcast.program,
-          },
-        });
-      }}
-      description={podcast.description}
-      author={podcast.author}
-    />
-    {isAddPlaylistModalOpen && (
-      <PlaylistList
-        onToggleModal={onToggleAddPlaylistModal}
-        podcast={podcast}
-      />
-    )}
-  </Wrapper>
-);
+	<Wrapper showsVerticalScrollIndicator={false} alwaysBounceVertical={false}>
+		<PodcastInfo imageURL={podcast.imageURL} subject={podcast.category} title={podcast.title} stars={4} />
+		<ActionButtons
+			onPressAddToPlaylist={onToggleAddPlaylistModal}
+			isDownloadingPodcast={isDownloadingPodcast}
+			onPressDownloadButton={onPressDownloadButton}
+			isPodcastDownloaded={isPodcastDownloaded}
+			onPressPlay={onPressPlay}
+		/>
+		<BottomContent
+			shouldShowAuthorSection={shouldShowAuthorSection}
+			onPressDetail={() => {
+				navigation.navigate(CONSTANTS.ROUTES.AUTHOR_DETAIL, {
+					[CONSTANTS.PARAMS.AUTHOR_DETAIL]: {
+						program: podcast.program,
+					},
+				})
+			}}
+			description={podcast.description}
+			author={podcast.program}
+		/>
+		{isAddPlaylistModalOpen && <PlaylistList onToggleModal={onToggleAddPlaylistModal} podcast={podcast} />}
+	</Wrapper>
+)
 
-export default PodcastDetailComponent;
+export default PodcastDetailComponent
