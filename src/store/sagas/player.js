76c8d8fb359875fa695acd2973301838ca8199c3
Suraj Shrestha <syuraj@gmail.com>
2021-04-17 23:13:57 -0400

    reverting previous changes; add podcast to recent list on pause

--- src/store/sagas/player.js
+++ src/store/sagas/player.js
@@ -92,8 +92,6 @@ const trackPlayerInit = async () => {
 			TrackPlayer.CAPABILITY_PLAY,
 			TrackPlayer.CAPABILITY_PAUSE,
 			TrackPlayer.CAPABILITY_STOP,
-			TrackPlayer.CAPABILITY_SEEK_TO,
-			TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
 			TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
 			TrackPlayer.CAPABILITY_JUMP_FORWARD,
 			TrackPlayer.CAPABILITY_JUMP_BACKWARD,
@@ -116,6 +114,9 @@ export function* play() {
 
 export function* pause() {
 	TrackPlayer.pause()
+
+	const { currentPodcast } = yield select((state) => state.player)
+	yield put(LocalPodcastsManagerCreators.addPodcastToRecentlyPlayedList(currentPodcast))
 }
 
 export function* jumpForward() {
